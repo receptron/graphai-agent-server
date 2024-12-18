15a906ec123f63b24ac098db62e7b7104f54a73e
isamu <isamu@to-kyo.to>
2024-12-18 14:44:57 +0900

    update extra server

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as llmAgents from "@graphai/llm_agents";
 import * as serviceAgents from "@graphai/service_agents";
 import * as vanillaAgents from "@graphai/vanilla";
 import * as sampleAgent from "./sample_agent";
+import * as extraAgents from "@graphai/extra-agents";
 
 import { agentDispatcher, agentsList } from "@receptron/graphai_express";
 
@@ -32,7 +33,7 @@ const logger = async (req: express.Request, res: express.Response, next: express
   next();
 };
 
-const agents = { ...llmAgents, ...serviceAgents, ...sampleAgent, ...vanillaAgents };
+const agents = { ...llmAgents, ...serviceAgents, ...sampleAgent, ...vanillaAgents, ...extraAgents };
 app.get(agentPath, agentsList(agents, hostName, agentPath));
 app.post(agentPath + "/:agentId", logger, agentDispatcher(agents));
 
